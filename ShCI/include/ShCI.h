58b1804ff392e831b010e417a9dd8cbaa046393e
Anas Boustani <68569053+MrSinho@users.noreply.github.com>
2021-12-18 01:58:17 +0100

    Log function

--- ShCI/include/ShCI.h
+++ ShCI/include/ShCI.h
@@ -1,6 +1,7 @@
 #ifndef SHCI_H
 #define SHCI_H
 
+#include <stdint.h>
 
 typedef enum shci_toolchain_flags {
     shci_c_toolchain = 0b0001,
@@ -15,13 +16,16 @@ typedef struct shci_github_repo_info {
     const char* repository_name;
     const char* access_token;
     const char* path;
+    char* logs[1024];
+    uint32_t sizes[1024];
+    uint32_t i;
 } shci_github_repo_info;
 
 
 
-extern void shci_call(const char* script, int pass_on_success, shci_github_repo_info info);
+extern void shci_call(const char* script, uint8_t pass_on_success, uint8_t log, shci_github_repo_info* info);
 
-extern void shci_get_toolchain(shci_toolchain_flags flags, shci_github_repo_info info);
+extern void shci_get_toolchain(shci_toolchain_flags flags, shci_github_repo_info* info);
 
 extern shci_github_repo_info shci_get_github_repo(const char* username, const char* repo_name, int recursive, const char* access_token, const char* path);
 
@@ -29,12 +33,14 @@ extern char* shci_read_text(const char* path);
 
 extern void shci_write_text(const char* buffer, const char* path);
 
-extern void shci_build_passing(const shci_github_repo_info info);
+extern void shci_log(shci_github_repo_info* info);
 
-extern void shci_build_failure(const shci_github_repo_info info);
+extern void shci_build_passing(shci_github_repo_info* info);
 
-extern void shci_end_success();
+extern void shci_build_failure(shci_github_repo_info* info);
 
-extern void shci_end_failure();
+extern void shci_end_success(shci_github_repo_info* info);
+
+extern void shci_end_failure(shci_github_repo_info* info);
 
 #endif // SHCI_H
--- ShCI/src/ShCI.c
+++ ShCI/src/ShCI.c
@@ -3,63 +3,153 @@
 #include <stdlib.h>
 #include <string.h>
 
-void shci_call(const char* script, int pass_on_success, shci_github_repo_info info) {
-    if (system(script) != 0) { 
-        shci_build_failure(info); 
-        shci_end_failure(); 
+#ifdef _MSC_VER
+#pragma warning (disable: 4996 4013 4047)
+#endif
+
+void shci_call(const char* script, uint8_t pass_on_success, uint8_t log, shci_github_repo_info* info) {
+    int exit_code = 0;
+    if (log) {
+        char call[528];
+        strcpy(call, "$$shci call$$:\n");
+        info->sizes[info->i] += strlen(call);
+        info->logs[info->i] = calloc(strlen(call), 1);
+        if (info->logs[info->i] == NULL) { return; }
+        strcpy(info->logs[info->i], call);
+        info->i++;
+        strcpy(call, script);
+        strcat(call, "\n");
+        info->sizes[info->i] += strlen(call);
+        info->logs[info->i] = calloc(strlen(call), 1);
+        if (info->logs[info->i] == NULL) { return; }
+        strcpy(info->logs[info->i], call);
+        info->i++;
+        
+        puts(call);
+#ifdef _MSC_VER
+        FILE* file_pipe = _popen(script, "r");
+#else
+        FILE* file_pipe = popen(script, "r");
+#endif // _MSC_VER
+        if (!file_pipe) { return; }
+
+        char _log[1024];
+        for (; fgets(_log, sizeof(_log), file_pipe)!=NULL; info->i++) {
+            info->sizes[info->i] = strlen(_log);
+            info->logs[info->i] = calloc(info->sizes[info->i], 1);
+            if (info->logs[info->i] == NULL) { break; }
+            strcpy(info->logs[info->i], _log);
+            printf("%s", _log);
+        }
+
+#ifdef _MSC_VER
+        exit_code = _pclose(file_pipe);
+#else 
+        exit_code = pclose(file_pipe);
+#endif // _MSC_VER
+    }
+    else { exit_code = system(script); }
+    if (exit_code != 0) { 
+        if (pass_on_success) {
+            shci_build_failure(info); 
+            shci_end_failure(info); 
+        }
         return;
     }
     if (pass_on_success) {
+        char path[256];
+        strcpy(path, info->path);
+#ifdef _WIN32
+        strcat(path, "/.ShCI/windows-log.md");
+#else
+        strcat(path, "/.ShCI/linux-log.md");
+#endif // _WIN32
+        FILE* stream = fopen(path, "w");
+        if (stream == NULL) { return; }
+        uint32_t offset = 0;
+        
+        char title[256];
+        strcpy(title, "# ");
+        strcat(title, info->repository_name);
+#ifdef _WIN32
+        strcat(title, "\n![windows_badge](windows-status.svg)\n## [Windows build logs:](https://github.com/MrSinho/ShCI)\n  ");
+#else 
+        strcat(title, "\n![linux_badge](linux-status.svg)\n## [Linux/Unix build logs:](https://github.com/MrSinho/ShCI)\n  ");
+#endif // _WIN32
+        fwrite(title, 1, strlen(title), stream);
+        
+        offset += strlen(title);
+        fseek(stream, offset, SEEK_SET);
+
+        const char* paragraph_start = "\n```bash\n";
+        const char* paragraph_end = "```\n";
+        const char* sig = "$$shci call$$:\n";
+        for (uint32_t i = 0; i < info->i; i++) {
+            if (strcmp(info->logs[i], sig) == 0) {
+                if (i != 0) {
+                    fwrite(paragraph_end, 1, strlen(paragraph_end), stream);
+                    offset += strlen(paragraph_end);
+                    fseek(stream, offset, SEEK_SET);
+                }
+                fwrite(paragraph_start, 1, strlen(paragraph_start), stream);
+                offset += strlen(paragraph_start);
+                fseek(stream, offset, SEEK_SET);
+            }
+            fwrite(info->logs[i], 1, info->sizes[i], stream);
+            offset += info->sizes[i];
+            fseek(stream, offset, SEEK_SET);
+        }
+        fwrite(paragraph_end, 1, strlen(paragraph_end), stream);
         shci_build_passing(info);
     }
 }
 
-void shci_get_toolchain(shci_toolchain_flags flags, shci_github_repo_info info) {
+void shci_get_toolchain(shci_toolchain_flags flags, shci_github_repo_info* info) {
 #ifdef _WIN32
     if (flags & shci_c_toolchain) {
-        system("choco install -y curl mingw cmake");
-        system("mingw-get install gcc mingw32-make gdb");    
+        shci_call("choco install -y curl mingw cmake", 0, 1, info);
+        shci_call("mingw-get install gcc mingw32-make gdb", 0, 1, info);    
     }
     if (flags & shci_cxx_toolchain) {
-        system("choco install -y curl mingw cmake");
-        system("mingw-get install g++ mingw32-make gdb");    
+        shci_call("choco install -y curl mingw cmake", 0, 1, info);
+        shci_call("mingw-get install g++ mingw32-make gdb", 0, 1, info);    
     }
     if (flags & shci_python2_toolchain) {
-        system("choco install -y curl python2");    
+        shci_call("choco install -y curl python2", 0, 1, info);    
     }
     if (flags & shci_python3_toolchain) {
-        system("choco install -y curl python3");    
+        shci_call("choco install -y curl python3", 0, 1, info);    
     }
 #else 
     if (flags & shci_c_toolchain) {
-        system("apt install -y curl gcc cmake make gdb");    
+        shci_call("apt install -y curl gcc cmake make gdb", 0, 1, info);    
     }
     if (flags & shci_cxx_toolchain) {
-        system("apt install -y curl g++ cmake make gdb");        
+        shci_call("apt install -y curl g++ cmake make gdb", 0, 1, info);        
     }
     if (flags & shci_python2_toolchain) {
-        system("apt install -y curl python2");        
+        shci_call("apt install -y curl python2", 0, 1, info);        
     }
     if (flags & shci_python3_toolchain) {
-        system("apt install -y curl python3");        
+        shci_call("apt install -y curl python3", 0, 1, info);        
     }
 #endif // __linux__    
     
 }
 
 shci_github_repo_info shci_get_github_repo(const char* username, const char* repo_name, int recursive, const char* access_token, const char* path) {
-//    char del[256];
-//#ifdef _WIN32
-//    strcpy(del, "rmdir -r ");
-//#else
-//    strcpy(del, "rm -rf ");
-//#endif
-//    strcat(del, path);
-//    system(del);
+    char del[256];
 #ifdef _WIN32
-    system("choco install -y git");
+    strcpy(del, "rmdir -r ");
 #else
-    system("apt install -y git-core");
+    strcpy(del, "rm -rf ");
+#endif
+    strcat(del, path);
+    shci_call(del, 0, 0, NULL);
+#ifdef _WIN32
+    shci_call("choco install -y git", 0, 0, NULL);
+#else
+    shci_call("apt install -y git-core", 0, 1, NULL);
 #endif // _WIN32
     char clone[1024];
     (recursive) ? strcpy(clone, "git clone --recursive https://") : strcpy(clone, "git clone https://");
@@ -70,13 +160,13 @@ shci_github_repo_info shci_get_github_repo(const char* username, const char* rep
     strcat(clone, repo_name);
     strcat(clone, " ");
     strcat(clone, path);
-    shci_github_repo_info info = { username, repo_name, access_token, path };
-    shci_call(clone, 0, info);
+    shci_github_repo_info info = { username, repo_name, access_token, path, 0, 0, 0 };
+    shci_call(clone, 0, 0, &info);
     char pull[256];
     strcpy(pull, "cd ");
     strcat(pull, path);
     strcat(pull, " && git pull");
-    shci_call(pull, 0, info);
+    shci_call(pull, 0, 0, &info);
     return info;
 } 
 
@@ -106,14 +196,18 @@ void shci_write_text(const char* buffer, const char* path) {
     free(stream);
 }
 
-void shci_build_passing(const shci_github_repo_info info) {
+void shci_log(shci_github_repo_info* info) {
+
+}
+
+void shci_build_passing(shci_github_repo_info* info) {
     char set[1024];
     strcpy(set, "cd ");
-    strcat(set, info.path);
+    strcat(set, info->path);
     strcat(set, " && mkdir .ShCI");
-    system(set);
+    shci_call(set, 0, 0, info);
     strcpy(set, "cd ");
-    strcat(set, info.path);
+    strcat(set, info->path);
     strcat(set, " && git config user.name \"ShCI\" && git config user.email \"none\"");
 #ifdef _WIN32
     strcat(set, " && cd .ShCI && curl https://img.shields.io/badge/windows-passing-green.svg -o windows-status.svg");
@@ -121,22 +215,22 @@ void shci_build_passing(const shci_github_repo_info info) {
     strcat(set, " && cd .ShCI && curl https://img.shields.io/badge/linux-passing-green.svg -o linux-status.svg");
 #endif
     strcat(set, " && cd .. && git add .ShCI && git commit -m \"ShCI status\" && git push https://");
-    strcat(set, info.access_token);
+    strcat(set, info->access_token);
     strcat(set, "@github.com/");
-    strcat(set, info.username);
+    strcat(set, info->username);
     strcat(set, "/");
-    strcat(set, info.repository_name);
-    system(set);
+    strcat(set, info->repository_name);
+    shci_call(set, 0, 0, info);
 }
 
-void shci_build_failure(const shci_github_repo_info info) {
+void shci_build_failure(shci_github_repo_info* info) {
     char set[1024];
     strcpy(set, "cd ");
-    strcat(set, info.path);
+    strcat(set, info->path);
     strcat(set, " && mkdir .ShCI");
-    system(set);
+    shci_call(set, 0, 0, NULL);
     strcpy(set, "cd ");
-    strcat(set, info.path);
+    strcat(set, info->path);
     strcat(set, " && git config user.name \"ShCI\" && git config user.email \"none\"");
 #ifdef _WIN32
     strcat(set, " && cd .ShCI && curl https://img.shields.io/badge/windows-failing-red.svg -o windows-status.svg");
@@ -144,18 +238,18 @@ void shci_build_failure(const shci_github_repo_info info) {
     strcat(set, " && cd .ShCI && curl https://img.shields.io/badge/linux-failing-red.svg -o linux-status.svg");
 #endif
     strcat(set, "&& cd .. && git add .ShCI && git commit -m \"ShCI status\" && git push https://");
-    strcat(set, info.access_token);
+    strcat(set, info->access_token);
     strcat(set, "@github.com/");
-    strcat(set, info.username);
+    strcat(set, info->username);
     strcat(set, "/");
-    strcat(set, info.repository_name);
-    system(set);
+    strcat(set, info->repository_name);
+    shci_call(set, 0, 0, NULL);
 }
 
-void shci_end_success() {
+void shci_end_success(shci_github_repo_info* info) {
     printf("shci message: all scripts have been executed successfully\n");
 }
 
-void shci_end_failure() {
+void shci_end_failure(shci_github_repo_info* info) {
     printf("shci message: failed running scripts\n");
 }
