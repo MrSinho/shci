009b054a18898885589c9ce94b6cdd8843c9f9ae
Anas Boustani <68569053+MrSinho@users.noreply.github.com>
2021-12-18 22:22:43 +0100

    Update ShCI.c

--- ShCI/src/ShCI.c
+++ ShCI/src/ShCI.c
@@ -2,105 +2,116 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #ifdef _MSC_VER
 #pragma warning (disable: 4996 4013 4047)
 #endif
 
 void shci_call(const char* script, uint8_t pass_on_success, uint8_t log, shci_github_repo_info* info) {
+    assert(script != NULL && info != NULL);
     int exit_code = 0;
-    if (log) {
-        char call[528];
-        strcpy(call, "$$shci call$$:\n");
-        info->sizes[info->i] += strlen(call);
-        info->logs[info->i] = calloc(strlen(call), 1);
-        if (info->logs[info->i] == NULL) { return; }
-        strcpy(info->logs[info->i], call);
-        info->i++;
-        strcpy(call, script);
-        strcat(call, "\n");
-        info->sizes[info->i] += strlen(call);
-        info->logs[info->i] = calloc(strlen(call), 1);
-        if (info->logs[info->i] == NULL) { return; }
-        strcpy(info->logs[info->i], call);
-        info->i++;
+    if (!log) { 
+        exit_code = system(script); 
+    }
+    else {
+        const char* call = "$shci call$\n";
+        {
+            info->sizes[info->i] = (uint32_t)strlen(call);
+            info->logs[info->i] = calloc(strlen(call), 1);
+            assert(info->logs[info->i] != NULL);
+            memcpy(info->logs[info->i], call, strlen(call));
+            info->i++;
+        }
         
-        puts(call);
-#ifdef _MSC_VER
-        FILE* file_pipe = _popen(script, "r");
-#else
-        FILE* file_pipe = popen(script, "r");
-#endif // _MSC_VER
-        if (!file_pipe) { return; }
-
-        char _log[1024];
-        for (; fgets(_log, sizeof(_log), file_pipe)!=NULL; info->i++) {
-            info->sizes[info->i] = strlen(_log);
-            info->logs[info->i] = calloc(info->sizes[info->i], 1);
-            if (info->logs[info->i] == NULL) { break; }
-            strcpy(info->logs[info->i], _log);
-            printf("%s", _log);
+        {
+            info->sizes[info->i] = (uint32_t)strlen(script)+1;
+            info->logs[info->i] = calloc(strlen(script)+1, 1);
+            assert(info->logs[info->i] != NULL);
+            memcpy(info->logs[info->i], script, strlen(script));
+            info->logs[info->i][strlen(script)] = '\n';
+            info->i++;
+            puts(call);
         }
 
+        {
 #ifdef _MSC_VER
-        exit_code = _pclose(file_pipe);
-#else 
-        exit_code = pclose(file_pipe);
+            FILE* fp = _popen(script, "r");
+#else
+            FILE* fp = popen(script, "r");
 #endif // _MSC_VER
+            assert(fp != NULL);
+            char output[1024];
+            for (; fgets(output, sizeof(output), fp) != NULL; info->i++) {
+                info->sizes[info->i] = (uint32_t)strlen(output);
+                info->logs[info->i] = calloc(strlen(output), 1);
+                assert(info->logs[info->i] != NULL);
+                memcpy(info->logs[info->i], output, strlen(output));
+                printf("%s", output);
+            }
+#ifdef _MSC_VER
+            exit_code = _pclose(fp);
+#else 
+            exit_code = pclose(fp);
+#endif // _MSC_VER
+        }
+
+        if (pass_on_success) {
+            char path[528];
+            strcpy(path, info->path);
+#ifdef _WIN32
+            strcat(path, "/.ShCI/windows-log.md");
+#else
+            strcat(path, "/.ShCI/linux-log.md");
+#endif // _WIN32
+
+            FILE* stream = fopen(path, "w");
+            assert(stream != NULL);
+
+            uint32_t offset = 0;
+
+            char title[256];
+            strcpy(title, "# ");
+            strcat(title, info->repository_name);
+#ifdef _WIN32
+            strcat(title, "\n![windows_badge](windows-status.svg)\n## [Windows build logs:](https://github.com/MrSinho/ShCI)\n  ");
+#else 
+            strcat(title, "\n![linux_badge](linux-status.svg)\n## [Linux/Unix build logs:](https://github.com/MrSinho/ShCI)\n  ");
+#endif // _WIN32
+            fwrite(title, 1, strlen(title), stream);
+
+            offset += (uint32_t)strlen(title);
+            fseek(stream, offset, SEEK_SET);
+
+            const char* paragraph_start = "\n```bash";
+            const char* paragraph_end = "\n```\n";
+            for (uint32_t i = 0; i < info->i; i++) {
+                if (memcmp(info->logs[i], call, strlen(call)) == 0) {
+                    if (i != 0) {
+                        fwrite(paragraph_end, 1, strlen(paragraph_end), stream);
+                        offset += (uint32_t)strlen(paragraph_end);
+                        fseek(stream, offset, SEEK_SET);
+                    }
+                    fwrite(paragraph_start, 1, strlen(paragraph_start), stream);
+                    offset += (uint32_t)strlen(paragraph_start);
+                    fseek(stream, offset, SEEK_SET);
+                }
+                fwrite(info->logs[i], 1, info->sizes[i], stream);
+                offset += info->sizes[i];
+                fseek(stream, offset, SEEK_SET);
+            }
+            fwrite(paragraph_end, 1, strlen(paragraph_end), stream);
+        }
     }
-    else { exit_code = system(script); }
     if (exit_code != 0) { 
         if (pass_on_success) {
             shci_build_failure(info); 
             shci_end_failure(info); 
         }
-        return;
     }
-    if (pass_on_success) {
-        char path[256];
-        strcpy(path, info->path);
-#ifdef _WIN32
-        strcat(path, "/.ShCI/windows-log.md");
-#else
-        strcat(path, "/.ShCI/linux-log.md");
-#endif // _WIN32
-        FILE* stream = fopen(path, "w");
-        if (stream == NULL) { return; }
-        uint32_t offset = 0;
-        
-        char title[256];
-        strcpy(title, "# ");
-        strcat(title, info->repository_name);
-#ifdef _WIN32
-        strcat(title, "\n![windows_badge](windows-status.svg)\n## [Windows build logs:](https://github.com/MrSinho/ShCI)\n  ");
-#else 
-        strcat(title, "\n![linux_badge](linux-status.svg)\n## [Linux/Unix build logs:](https://github.com/MrSinho/ShCI)\n  ");
-#endif // _WIN32
-        fwrite(title, 1, strlen(title), stream);
-        
-        offset += strlen(title);
-        fseek(stream, offset, SEEK_SET);
-
-        const char* paragraph_start = "\n```bash\n";
-        const char* paragraph_end = "```\n";
-        const char* sig = "$$shci call$$:\n";
-        for (uint32_t i = 0; i < info->i; i++) {
-            if (strcmp(info->logs[i], sig) == 0) {
-                if (i != 0) {
-                    fwrite(paragraph_end, 1, strlen(paragraph_end), stream);
-                    offset += strlen(paragraph_end);
-                    fseek(stream, offset, SEEK_SET);
-                }
-                fwrite(paragraph_start, 1, strlen(paragraph_start), stream);
-                offset += strlen(paragraph_start);
-                fseek(stream, offset, SEEK_SET);
-            }
-            fwrite(info->logs[i], 1, info->sizes[i], stream);
-            offset += info->sizes[i];
-            fseek(stream, offset, SEEK_SET);
-        }
-        fwrite(paragraph_end, 1, strlen(paragraph_end), stream);
-        shci_build_passing(info);
+    else {
+        if (pass_on_success) { shci_build_passing(info); }
+        shci_end_success(info);
     }
 }
 
@@ -145,12 +156,7 @@ shci_github_repo_info shci_get_github_repo(const char* username, const char* rep
     strcpy(del, "rm -rf ");
 #endif
     strcat(del, path);
-    shci_call(del, 0, 0, NULL);
-#ifdef _WIN32
-    shci_call("choco install -y git", 0, 0, NULL);
-#else
-    shci_call("apt install -y git-core", 0, 0, NULL);
-#endif // _WIN32
+    system(del);
     char clone[1024];
     (recursive) ? strcpy(clone, "git clone --recursive https://") : strcpy(clone, "git clone https://");
     strcat(clone, access_token);
@@ -161,12 +167,12 @@ shci_github_repo_info shci_get_github_repo(const char* username, const char* rep
     strcat(clone, " ");
     strcat(clone, path);
     shci_github_repo_info info = { username, repo_name, access_token, path, 0, 0, 0 };
-    shci_call(clone, 0, 0, &info);
+    system(clone);
     char pull[256];
     strcpy(pull, "cd ");
     strcat(pull, path);
     strcat(pull, " && git pull");
-    shci_call(pull, 0, 0, &info);
+    system(pull);
     return info;
 } 
 
@@ -205,7 +211,7 @@ void shci_build_passing(shci_github_repo_info* info) {
     strcpy(set, "cd ");
     strcat(set, info->path);
     strcat(set, " && mkdir .ShCI");
-    shci_call(set, 0, 0, info);
+    system(set);
     strcpy(set, "cd ");
     strcat(set, info->path);
     strcat(set, " && git config user.name \"ShCI\" && git config user.email \"none\"");
@@ -220,7 +226,7 @@ void shci_build_passing(shci_github_repo_info* info) {
     strcat(set, info->username);
     strcat(set, "/");
     strcat(set, info->repository_name);
-    shci_call(set, 0, 0, info);
+    system(set);
 }
 
 void shci_build_failure(shci_github_repo_info* info) {
@@ -228,7 +234,7 @@ void shci_build_failure(shci_github_repo_info* info) {
     strcpy(set, "cd ");
     strcat(set, info->path);
     strcat(set, " && mkdir .ShCI");
-    shci_call(set, 0, 0, NULL);
+    system(set);
     strcpy(set, "cd ");
     strcat(set, info->path);
     strcat(set, " && git config user.name \"ShCI\" && git config user.email \"none\"");
@@ -243,7 +249,7 @@ void shci_build_failure(shci_github_repo_info* info) {
     strcat(set, info->username);
     strcat(set, "/");
     strcat(set, info->repository_name);
-    shci_call(set, 0, 0, NULL);
+    system(set);
 }
 
 void shci_end_success(shci_github_repo_info* info) {
