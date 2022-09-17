fbfe5d6aebe507adbdb87c6a84888a61161c2d67
Anas Boustani <sino3791@gmail.com>
2022-09-18 01:34:54 +0200

    Update shci.py

--- shci/python/shci.py
+++ shci/python/shci.py
@@ -108,7 +108,7 @@ def shci_build_status(repo:shci_github_repo_info, status:bool):
     print(f"shci: {setup_log_dir}")
     os.system(setup_log_dir)
 
-    badge_file:TextIOWrapper = open(f".shci/{repo._os}-status.svg", "wb")
+    badge_file:TextIOWrapper = open(f"{repo.dir}/.shci/{repo._os}-status.svg", "wb")
     if (status == True):#success
         print("shci: Build success\n")
         clone_badge:Response = requests.get("https://img.shields.io/badge/{repo._os}-passing-green.svg", )
